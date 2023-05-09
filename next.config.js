7927beb71b1ccbbb61e22ea76bf302167fe1ca0c
nilesjoel <nilesjoel@gmail.com>
2023-05-09 13:38:55 -0500

    Testing

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+
+  // This uses phases as outlined here: https://nextjs.org/docs/#custom-configuration
+  module.exports = (phase) => {
+    
+  
+    // next.config.js object
+    return {
+      reactStrictMode: true,
+      compiler: {
+        styledComponents: true,
+      },
+      images: {
+        domains: ["res.cloudinary.com"],
+      },
+      env,
+    }
+  }
